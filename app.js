528ca379307f7f888995bb1e6f0c05f9f02f877e
Viktor Uhnyuck <uhviktor@gmail.com>
2016-04-11 02:31:59 -0400

    Changed node js code

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var server = http.createServer(function (req, res) {
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+//server.listen(port);
 
 // Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+//console.log('Server running at http://127.0.0.1:' + port + '/');

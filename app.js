98ba685e1c9b6876d0ea6b11e1feec3474340d7c
Viktor Uhnyuck <uhviktor@gmail.com>
2016-04-11 03:43:35 -0400

    Changed to generic color html and simple http request for server

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+var sys = require("sys"),
+my_http = require("http");
+fs = require('fs');
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }  
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
-
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
-
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+my_http.createServer(function(request,response){
+    sys.puts("I got kicked");
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(html);  
+    response.end();
+}).listen(8080);
 });
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-//server.listen(port);
-
-// Put a friendly message on the terminal
-//console.log('Server running at http://127.0.0.1:' + port + '/');
+sys.puts("Server Running on 8080"); 
